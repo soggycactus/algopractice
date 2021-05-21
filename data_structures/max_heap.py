343c63acb58e3a7e9b6446272ff2aad662b95291
LCStephens <lcstephens@utexas.edu>
2021-05-21 12:33:30 -0500

    fix heap structures to use single heapify method, implement max heap

--- data_structures/max_heap.py
+++ data_structures/max_heap.py
@@ -1,14 +1,15 @@
-""" MinHeap Data Structure """
-from random import randint
+""" MaxHeap Data Structure """
 
 
-class MinHeap:
-    """MinHeap"""
+class MaxHeap:
+    """
+    MaxHeap implements the heap data structure
+    """
 
     def __init__(self, items: list = []):
         super().__init__()
         self.heap = items.copy()
-        self.build_min_heap()
+        self.build_max_heap()
 
     def __get_left_child_index(self, parent_index: int) -> int:
         return 2 * parent_index + 1
@@ -19,13 +20,13 @@ class MinHeap:
     def __get_parent_index(self, child_index: int) -> int:
         return (child_index - 1) // 2
 
-    def __has_left_child(self, index: int) -> bool:
+    def __has_left_child(self, index: int) -> int:
         return self.__get_left_child_index(index) < len(self.heap)
 
-    def __has_right_child(self, index: int) -> bool:
+    def __has_right_child(self, index: int) -> int:
         return self.__get_right_child_index(index) < len(self.heap)
 
-    def __has_parent(self, index: int) -> bool:
+    def __has_parent(self, index: int) -> int:
         return self.__get_parent_index(index) >= 0
 
     def __swap(self, index_one: int, index_two: int):
@@ -35,75 +36,73 @@ class MinHeap:
 
     def peek(self) -> int:
         """
-        Returns the parent node of the heap
+        peek returns the parent node of the heap
         """
         return self.heap[0]
 
     def poll(self) -> int:
         """
-        Pops the parent node of the heap & rebuilds the heap
+        poll extracts the parent node of the heap
         """
-        poll = self.heap[0]
-        self.heap[0] = self.heap[len(self.heap) - 1]
+        parent = self.heap[0]
+        self.__swap(0, -1)
         del self.heap[-1]
-        self.min_heapify(0)
-        return poll
+        self.max_heapify(0)
+        return parent
 
-    def min_heapify(self, index: int):
+    def max_heapify(self, index: int) -> int:
         """
-        recursively builds a max heap from an unordered array
+        max_heapify recursively builds a max heap from an unordered array
         """
         left = self.__get_left_child_index(index)
         right = self.__get_right_child_index(index)
 
-        if self.__has_left_child(index) and self.heap[left] < self.heap[index]:
-            smallest = left
-
+        if self.__has_left_child(index) and self.heap[left] > self.heap[index]:
+            largest = left
         else:
-            smallest = index
+            largest = index
 
-        if self.__has_right_child(index) and self.heap[right] < self.heap[smallest]:
-            smallest = right
+        if self.__has_right_child(index) and self.heap[right] > self.heap[largest]:
+            largest = right
 
-        if smallest != index:
-            self.__swap(index, smallest)
-            self.min_heapify(smallest)
+        if largest != index:
+            self.__swap(index, largest)
+            self.max_heapify(largest)
 
-    def build_min_heap(self):
+    def build_max_heap(self):
         """
-        build_min_heap calls max_heapify to construct a max heap
+        build_max_heap calls max_heapify to construct a max heap
 
-        we call min_heapify from the range (len(n) // 2) - 1
+        we call max_heapify from the range (len(n) // 2) - 1
         because these indices are guaranteed to be nodes with children
         """
         last_node = len(self.heap) // 2
         # last_node minus one because of zero-based indexing
         for i in range(last_node - 1, -1, -1):
-            self.min_heapify(i)
+            self.max_heapify(i)
 
     def add(self, item: int):
         """
-        Adds a new item to the heap
+        add inserts a new value into the heap
         """
         self.heap.append(item)
-        self.min_heapify(0)
+        self.max_heapify(0)
 
 
 def main():
     """
     HeapSort algorithm using MinHeap
     """
-    # array = [54, 1, 3, 2, 5, 4, 8, 65, 47]
-    array = [randint(0, 100000000000) for _ in range(1000000)]
-    heap = MinHeap(array)
+    array = [54, 1, 3, 2, 5, 4, 8, 65, 47]
+    heap = MaxHeap(array)
     sorted_array = []
 
     for _ in range(0, len(array)):
         value = heap.poll()
         sorted_array.append(value)
 
-    # print(array)
-    # print(sorted_array)
+    print(array)
+    print(sorted_array)
 
 
 if __name__ == "__main__":
