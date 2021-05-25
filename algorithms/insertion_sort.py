59fd797612f4f91682afaa698cef1f3b6cd3e408
LCStephens <lcstephens@utexas.edu>
2021-05-25 11:50:10 -0500

    clean up insertion sort

--- algorithms/insertion_sort.py
+++ algorithms/insertion_sort.py
@@ -5,16 +5,13 @@ def insertion_sort(array: list) -> list:
     """
     Implements the insertion sort algorithm
     """
-    for i in range(0, len(array)):
-        for j in range(i + 1, len(array)):
-            first = array[i]
-            second = array[j]
-
-            if first > second:
-                array[i] = second
-                array[j] = first
-
-    return array
+    # [2,5,4,7,1,2]
+    for i in range(1, len(array)):
+        while i > 0 and array[i - 1] > array[i]:
+            temp = array[i]
+            array[i] = array[i - 1]
+            array[i - 1] = temp
+            i -= 1
 
 
 def main():
@@ -23,7 +20,8 @@ def main():
     """
     array = [5, 1, 3, 2, 3, 6, 8, 2, 4, 8, 90, 2, 3]
     print(array)
-    print(insertion_sort(array))
+    insertion_sort(array)
+    print(array)
 
 
 if __name__ == "__main__":
