df7e723975266351d9eba206af5dd0be414315c9
LCStephens <lcstephens@utexas.edu>
2021-06-08 17:10:19 -0500

    binary search tree data structure

---
+++ data_structures/binary_search_tree.py
@@ -0,0 +1,159 @@
+""" Binary Search Tree """
+
+
+class BinarySearchTreeNode:
+    """
+    BinarySearchTreeNode implements a BST
+    """
+
+    def __init__(self, data: int) -> None:
+        self.data = data
+        self.left = None
+        self.right = None
+        self.parent = None
+
+    def insert(self, data: int) -> None:
+        """
+        Inserts a new element into the BST
+        """
+        if data <= self.data:
+            if self.left is None:
+                self.left = BinarySearchTreeNode(data)
+                self.left.parent = self
+            else:
+                self.left.insert(data)
+        else:
+            if self.right is None:
+                self.right = BinarySearchTreeNode(data)
+                self.right.parent = self
+            else:
+                self.right.insert(data)
+
+    def inorder_tree_walk(self):
+        """
+        Performs an inorder tree walk
+        """
+        if self.left is not None:
+            self.left.inorder_tree_walk()
+        print(self.data)
+        if self.right is not None:
+            self.right.inorder_tree_walk()
+
+    def find(self, value: int):
+        """
+        Searches the BST for a value
+        """
+        while self.data != value:
+            if value < self.data and self.left is not None:
+                return self.left.find(value)
+            elif value > self.data and self.right is not None:
+                return self.right.find(value)
+            else:
+                return None
+
+        return self
+
+    def min(self):
+        """
+        Returns the minumum element of the BST
+        """
+        x = self
+        while x.left is not None:
+            x = x.left
+        return x.data
+
+    def max(self):
+        """
+        Returns the maximum element of the BST
+        """
+        x = self
+        while x.right is not None:
+            x = x.right
+        return x.data
+
+
+def tree_minimum(node: BinarySearchTreeNode):
+    """
+    Returns the minimum element of a BST
+    """
+    while node.left is not None:
+        node = node.left
+    return node
+
+
+def tree_maximum(node: BinarySearchTreeNode):
+    """
+    Returns the maximum element of a BST
+    """
+    while node.right is not None:
+        node = node.right
+    return node
+
+
+def find_successor(node: BinarySearchTreeNode):
+    """
+    Returns the successor of a given BST node
+    """
+    if node.right is not None:
+        return tree_minimum(node.right)
+
+    parent = node.parent
+    while parent is not None and parent.right == node:
+        node = parent
+        parent = node.parent
+
+    return parent
+
+
+def find_predecessor(node: BinarySearchTreeNode):
+    """
+    Returns the predecessor of a given BST node
+    """
+    if node.left is not None:
+        return tree_maximum(node.left)
+
+    parent = node.parent
+    while parent is not None and parent.left == node:
+        node = parent
+        parent = node.parent
+
+    return parent
+
+
+def main():
+    """
+    Example Usage
+    """
+    bst = BinarySearchTreeNode(15)
+    bst.insert(6)
+    bst.insert(18)
+    bst.insert(3)
+    bst.insert(7)
+    bst.insert(17)
+    bst.insert(20)
+    bst.insert(2)
+    bst.insert(4)
+    bst.insert(13)
+    bst.insert(9)
+
+    print("--- Inorder Tree Walk ---")
+    bst.inorder_tree_walk()
+    print("--- Find Element ---")
+    find = bst.find(13)
+    print(find.data)
+    print("--- Maximum ---")
+    print(bst.max())
+    print("--- Minimum ---")
+    print(bst.min())
+
+    print("--- Successor ---")
+    successor = find_successor(find)
+    print(successor.data)
+
+    print("--- Predecessor ---")
+    predecessor = find_predecessor(find)
+    print(predecessor.data)
+
+
+if __name__ == "__main__":
+    main()
